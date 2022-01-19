42e368875f081f14c5a4ba71408914b6537ac5ce
vhoang11902 <tvhoang11902@gmail.com>
2022-01-20 00:34:16 +0700

    sneakershop

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector('.aspect-ratio-169')
+const dotItem = document.querySelectorAll(".dot")
+let imgNuber = imgPosition.length
+let index = 0
+imgPosition.forEach(function(image,index){
+    image.style.left = index*100 +"%" 
+    dotItem[index].addEventListener("click",function(){
+    slider (index)
+    })    
+})
+function imgSlide (){
+    index++;
+    console.log(index)
+    if (index >= imgNuber) {index = 0}
+    slider (index)
+}
+function slider(index){
+    imgContainer.style.left = "-" + index*100 + "%"
+    const dotActive = document.querySelector('.active')
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active")
+}
+setInterval(imgSlide,5000)
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
+itemsliderbar.forEach(function(menu,index){
+   menu.addEventListener("click",function(){
+    menu.classList.toggle("block")
+   })
+})
+const header = document.querySelector('.top');
+window.onscroll = function(){
+    var top = window.scrollY;
+    console.log(top);
+    if (top>= 30){
+        header.classList.add('active1')
+    }else{
+        header.classList.remove('active1')
+    }
+}
